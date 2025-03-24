13cfa954d6b19175157ed1122a818438edb605e5
reigadegr <272268642@qq.com>
2025-03-25 02:52:20 +0800

    chore: 修剪module

--- module/service.sh
+++ module/service.sh
@@ -9,13 +9,6 @@ wait_until_login() {
     until [ -d /sdcard/Android ]; do sleep 1; done
 }
 
-contlict_remover() {
-    [ ! -d "$1" ] && return
-    chattr -R -ia $1
-    rm -rf $1
-    killall -9 $2
-}
-
 lock_val() {
     find "$2" -type f | while read -r file; do
         file="$(realpath "$file")"
@@ -27,29 +20,14 @@ lock_val() {
     done
 }
 
-reset_freq(){
-    for i in /sys/devices/system/cpu/cpu?/cpufreq/cpuinfo_max_freq; do
-        lock_val $(cat $i) $(dirname "$i")/scaling_max_freq
-    done
-
-    for i in /sys/devices/system/cpu/cpu?/cpufreq/cpuinfo_min_freq; do
-        lock_val $(cat $i) $(dirname "$i")/scaling_min_freq
-    done
-}
-
-
 if [ "$(getprop sys.boot_completed)" != "1" ]; then
-    contlict_remover "/data/adb/modules/AppOpt" "AppOpt"
     wait_until_login
-    if [ ! -L $MODDIR/thread_opt.toml ]; then
-        rm $MODDIR/thread_opt.toml
-        ln -s /storage/emulated/0/Android/thread_opt.toml $MODDIR/thread_opt.toml
+    if [ ! -L $MODDIR/naughty_apps.toml ]; then
+        rm $MODDIR/naughty_apps.toml
+        ln -s /storage/emulated/0/Android/naughty_apps.toml $MODDIR/naughty_apps.toml
     fi
-    stop oiface gameopt_hal_service-1-0 vendor.urcc-hal-aidl horae
-    killall -9 vendor.oplus.hardware.urcc-service vendor.oplus.hardware.gameopt-service oiface horae
-    reset_freq
 fi
 
-killall -15 thread-opt; rm $LOG
-chmod +x ${0%/*}/thread-opt
-RUST_BACKTRACE=1 nohup $MODDIR/thread-opt >$LOG 2>&1 &
+killall -15 freezer-rs; rm $LOG
+chmod +x ${0%/*}/freezer-rs
+RUST_BACKTRACE=1 nohup $MODDIR/freezer-rs >$LOG 2>&1 &
